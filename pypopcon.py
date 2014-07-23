29b5768a5cf70454ce171b766565bdcf1bf655b6
David Wahlstrom <dwahlstrom@isilon.com>
2014-07-23 11:13:58 -0700

    Cleanup usage message
    Usage message was borrowed from another python script and had some
    unrelated flags included.  These unrelated flags have been cleaned up.

--- pypopcon.py
+++ pypopcon.py
@@ -31,7 +31,7 @@ def get_options():
     """ command-line options """
     log.debug("in get_options()")
 
-    usage = "usage: %prog -f <FILE> [options]"
+    usage = "usage: %prog [options]"
     OptionParser = optparse.OptionParser
     parser = OptionParser(usage)
 
